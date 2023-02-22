7a6b22a35a341472075e1a3de309cd9ad59b6ebb
Javier Moreno <ejmoreno6@espe.edu.ec>
2023-02-22 14:49:54 -0500

    update script.js

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueID);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-clone-eyw2.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
